db6fa6020f74bae784cda0a458e8005c42e0bd48
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-01-20 15:28:57 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+categories = Category.create ([ 
+{name: "Fiction"},
+{name: "Editorial"},
+{name: "Documentary"},
+{name: "Biography"},
+{name: "Historical"},
+{name: "Romance"},
+{name: "Health"}
+
+])
+
+posts = Post.create ([
+  {title: "How to Lose Weight", description: "Eat less", category_id:1},
+  {title: "How to Ride a Bike", description: "Just Do It", category_id: 5},
+  {title: "How to Build a House", description: "Become an Apprentice", category_id: 4},
+  {title: "How to Boil Eggs", description: "13 minutes", category_id: 3},
+  
+])
